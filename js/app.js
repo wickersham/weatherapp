19f572c8fcbf6e9da3aac83d8a67869d4255348c
wickersham <wickersham90@gmail.com>
2015-10-02 19:46:42 -0400

    mid-feature. got layout, working on ngroute

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var app = angular.module("NgWeather3", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/home", {
+        templateUrl: "",
+    })
+
+});
+
+
+app.controller ("WeatherController3", function($scope) {
+
+});
+
