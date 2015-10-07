b5a50709ee2628dec03aeef1bc56d750467e9698
wickersham <wickersham90@gmail.com>
2015-10-06 20:48:17 -0400

    now with multiple apis

--- js/app.js
+++ js/app.js
@@ -1,15 +1,69 @@
-var app = angular.module("NgWeather3", ["ngRoute"]);
+var app = angular.module("ngWeather3", ["ngRoute"]);
 
 app.config(function($routeProvider) {
     $routeProvider
     .when("/home", {
-        templateUrl: "",
+        templateUrl: "html/home.html",
+        controller: "WeatherController3"
     })
+    
+    .when("/weather", {
+        templateUrl: "html/weather.html",
+        controller: "WeatherController3"
+    })
+    
+    .when("/about", {
+        templateUrl: "html/about.html",
+        controller: "WeatherController3"
+    })
+    
+    .otherwise({
+        redirectTo: "/home"
+    });
 
 });
 
-
-app.controller ("WeatherController3", function($scope) {
-
+//create weather service
+app.factory("weatherService3", function($http) {
+    var service = {};
+    var baseUrl = "https://api.forecast.io/forecast/";
+    var baseUrlwithwkey = baseUrl + wkey + "/";
+    
+//this gets the weather information
+    service.get = function(latitude, longitude) {
+    //http get
+        return $http.jsonp(baseUrlwithwkey + latitude + "," + longitude + "?callback=JSON_CALLBACK");
+    };
+    
+    return service;
+    
 });
 
+app.controller ("WeatherController3", function($scope, $http, weatherService3) {
+    
+    $scope.location = "";
+    $scope.weatherData = {};
+    
+    //getWeather function
+    $scope.getWeather = function() {
+        var latitude = 0;
+        var longitude = 0;
+        $http.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + $scope.location + "&key=" + gkey)
+            .success(function(response) {
+                latitude = response.results[0].geometry.location.lat;
+                longitude = response.results[0].geometry.location.lng;
+                console.log(latitude);
+                console.log(longitude);
+                    weatherService3.get(latitude, longitude)
+                        .success(function(response) {
+                        $scope.weatherData = response;
+                        })
+                        .error(function(error) {
+                        console.log(err);
+                        });
+                })
+            .error(function(error) {
+                        console.log(err);
+             });
+    };    
+});
---
+++ js/key.js
@@ -0,0 +1,2 @@
+var wkey = "3c3fd6d91c45ce00c6bb66c182e66e84";
+var gkey = "AIzaSyAlCvfQubEJF0kHjnk6Vo7tSZZg549KEOk";
